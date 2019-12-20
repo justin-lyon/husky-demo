ba7dc0f2aecec828b1ee997bf8ef04ea36cac527
Justin Lyon <jml6487@gmail.com>
2019-12-20 08:09:15 -0600

    installed mapbox/secret-shield dev

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,8 +2,8 @@ const tasks = arr => arr.join(' && ')
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks([
-      'echo \" this should fail\" && exit 1 | cat > logs/husky.log'
-    ])
+    // 'pre-commit': tasks([
+    //   'echo \" this should fail\" && exit 1 | cat > logs/husky.log'
+    // ])
   }
 }
